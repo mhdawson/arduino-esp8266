e89515131e6efa2dd6bb6e772d23ac443410089a
Michael Dawson <mdawson@devrus.com>
2016-12-16 22:23:26 -0500

    fix up spacing in blueline device

--- 433Rx/BluelineDevice.h
+++ 433Rx/BluelineDevice.h
@@ -1,4 +1,4 @@
-// Copyright 2014-2015 the project authors as listed in the AUTHORS file.
+// Copyright 2014-2016 the project authors as listed in the AUTHORS file.
 // All rights reserved. Use of this source code is governed by the
 // license that can be found in the LICENSE file.
 
@@ -10,23 +10,23 @@
 #define BITS_IN_MESSAGE 32
 
 class BluelineDevice : public Device {
-   public:
-      BluelineDevice(long houseCode);
+  public:
+    BluelineDevice(long houseCode);
 
-      virtual int deviceType(void);
-      virtual char* deviceName(void);
+    virtual int deviceType(void);
+    virtual char* deviceName(void);
 
-      virtual void processPulse(long duration);
-      virtual void decodeMessage(Message* message);
-      virtual void publishTopic(Message* message, char* buffer, int maxLength);
+    virtual void processPulse(long duration);
+    virtual void decodeMessage(Message* message);
+    virtual void publishTopic(Message* message, char* buffer, int maxLength);
 
-   private:
-      bool syncFound;
-      unsigned int bitCount;
-      unsigned int code;
-      unsigned int durations[BITS_IN_MESSAGE];
-      long houseCode;
-      long pulseCount;
+  private:
+    bool syncFound;
+    unsigned int bitCount;
+    unsigned int code;
+    unsigned int durations[BITS_IN_MESSAGE];
+    long houseCode;
+    long pulseCount;
 };
 
 #endif
