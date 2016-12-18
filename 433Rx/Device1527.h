07360f87011514c58f1ac7b27be260ad87371c55
Michael Dawson <mdawson@devrus.com>
2016-12-18 12:00:37 -0500

    fix up formatting for Device1527

--- 433Rx/Device1527.h
+++ 433Rx/Device1527.h
@@ -11,28 +11,28 @@
 #define BITS_IN_MESSAGE_1527 24
 
 class Device1527 : public Device, public MqttDevice {
-   public:
-      Device1527(int pulseWidth, int pulseSlack, int minRepeats,
-                 PubSubClient* client, char* topic);
+  public:
+    Device1527(int pulseWidth, int pulseSlack, int minRepeats,
+               PubSubClient* client, char* topic);
 
-      virtual int deviceType(void);
-      virtual char* deviceName(void);
+    virtual int deviceType(void);
+    virtual char* deviceName(void);
 
-      virtual void processPulse(long duration);
-      virtual void decodeMessage(Message* message);
-      virtual void publishTopic(int messageNum, Message* message, char* buffer, int maxLength);
-      
-   private:
-      int _pulseWidth;
-      int _pulseSlack;
-      int _minRepeats;
-      bool syncFound;
-      unsigned int bitCount;
-      unsigned char receivedCode[BITS_IN_MESSAGE_1527 + 1];
-      unsigned char lastMessage[BITS_IN_MESSAGE_1527];
-      unsigned int durations[BITS_IN_MESSAGE_1527];
-      long pulseCount;
-      long repeatCount;
+    virtual void processPulse(long duration);
+    virtual void decodeMessage(Message* message);
+    virtual void publishTopic(int messageNum, Message* message, char* buffer, int maxLength);
+
+  private:
+    int _pulseWidth;
+    int _pulseSlack;
+    int _minRepeats;
+    bool syncFound;
+    unsigned int bitCount;
+    unsigned char receivedCode[BITS_IN_MESSAGE_1527 + 1];
+    unsigned char lastMessage[BITS_IN_MESSAGE_1527];
+    unsigned int durations[BITS_IN_MESSAGE_1527];
+    long pulseCount;
+    long repeatCount;
 };
 
 #endif
