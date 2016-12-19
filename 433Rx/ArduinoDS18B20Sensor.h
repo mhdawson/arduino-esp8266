d3f03f2860b33582a441e05b861cc42420f63d1c
Michael Dawson <mdawson@devrus.com>
2016-12-19 14:25:47 -0500

    Fix up formatting in ArduinoDS18B20Sensor files

--- 433Rx/ArduinoDS18B20Sensor.h
+++ 433Rx/ArduinoDS18B20Sensor.h
@@ -11,28 +11,30 @@
 #define BITS_IN_MESSAGE 32
 
 class ArduinoDS18B20Sensor : public Device, public MqttDevice {
-   public:
-      ArduinoDS18B20Sensor(PubSubClient* client, char* topic);
+  public:
+    ArduinoDS18B20Sensor(PubSubClient* client, char* topic);
 
-      virtual int deviceType(void);
-      virtual char* deviceName(void);
+    virtual int deviceType(void);
+    virtual char* deviceName(void);
 
-      virtual void processPulse(long duration);
-      virtual void decodeMessage(Message* message);
-      virtual int numMessages(void);
-      virtual void getMessageText(int messageNum, Message* message, char* buffer, int maxLength);
-      virtual void publishTopic(Message* message, char* buffer, int maxLength);
-      virtual void publishTopic(int messageNum, Message* message, char* buffer, int maxLength);
+    virtual void processPulse(long duration);
+    virtual void decodeMessage(Message* message);
+    virtual int numMessages(void);
+    virtual void getMessageText(int messageNum, Message* message,
+                                char* buffer, int maxLength);
+    virtual void publishTopic(Message* message, char* buffer, int maxLength);
+    virtual void publishTopic(int messageNum, Message* message,
+                              char* buffer, int maxLength);
 
-   private:
-      bool syncFound;
-      unsigned int bitCount;
-      unsigned long code;
-      long syncCount;
-      unsigned int durations[BITS_IN_MESSAGE];
-      long houseCode;
-      long pulseCount;
-      bool validateChecksum(int code);
+  private:
+    bool syncFound;
+    unsigned int bitCount;
+    unsigned long code;
+    long syncCount;
+    unsigned int durations[BITS_IN_MESSAGE];
+    long houseCode;
+    long pulseCount;
+    bool validateChecksum(int code);
 };
 
 #endif
