81200c9d71d62d0a4eec80c121af359a680cbb97
Michael Dawson <mdawson@devrus.com>
2017-04-02 19:18:29 -0400

    doc: update WirelessCconfig.h sample

--- WirelessConfig.h
+++ WirelessConfig.h
@@ -3,5 +3,11 @@
 #define __WIRELESS_H__
 const char *ssid =  "XXXX";   // cannot be longer than 32 characters!
 const char *pass =  "YYYY";   //
+
+// setting for older projects
 const int mqttServer[] = { xx, xx, xx, xx };
+
+// values for newer projects
+const char* mqttServerString = "10.1.1.186";
+const uint16_t mqttServerPort = 1883;
 #endif
