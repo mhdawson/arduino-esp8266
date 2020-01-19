9040a5d580aac2ec382689780d71977a847ca7b3
Michael Dawson <mdawson@devrus.com>
2020-01-19 16:43:04 -0500

    src: flip plus/minux after rewire

--- PlatformIO/MqttScale/include/MqttScale.h
+++ PlatformIO/MqttScale/include/MqttScale.h
@@ -2,5 +2,5 @@
 #define __MQTT_SCALE_H__
 #define SCALE_TOPIC "house/scale/1/weight"
 #define SCALE_TARE_TOPIC "house/scale/1/tare"
-#define SCALE_MULTIPLIER -384.1
+#define SCALE_MULTIPLIER 384.1
 #endif
